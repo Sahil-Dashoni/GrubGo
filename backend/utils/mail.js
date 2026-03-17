3c81f2983b66d968c196625e16bc6588f497b8e9
Sahil Dashoni <sahildashoni453@gmail.com>
2026-03-17 21:43:33 +0530

    again edited mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,39 +1,43 @@
-import * as SibApiV3Sdk from '@getbrevo/brevo';
+import Brevo from "@getbrevo/brevo";
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+const client = new Brevo.TransactionalEmailsApi();
 
-const apiKey = apiInstance.authentications['apiKey'];
-apiKey.apiKey = process.env.BREVO_API_KEY;
-
+client.setApiKey(
+  Brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
 export const sendOtpMail = async (to, otp) => {
-    try {
-        await apiInstance.sendTransacEmail({
-            sender: { email: process.env.EMAIL },
-            to: [{ email: to }],
-            subject: "Reset Your Password",
-            html:`<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
-        });
+  try {
+    await client.sendTransacEmail({
+      sender: { email: process.env.EMAIL },
+      to: [{ email: to }],
+      subject: "Reset Password",
+      htmlContent: `<p>Your OTP for password reset is <b>${otp}</b>. It expires in 5 minutes.</p>`
+    });
 
-        console.log("MAIL SENT:", info.response);
+    console.log("MAIL SENT ✅");
 
-    } catch (error) {
-        console.log("MAIL ERROR:", error); 
-        throw error;
-    }
+  } catch (error) {
+    console.log("BREVO ERROR:", error);
+    throw error;
+  }
 };
 
-
-export const sendDeliveryOtpMail=async (user,otp) => {
-    try {
-        await apiInstance.sendTransacEmail({
-            sender: { email: process.env.EMAIL },
-            to: [{ email: user.email }],
-        subject:"Delivery OTP",
-        html:`<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
+export const sendDeliveryOtpMail = async (user, otp) => {
+  try {
+    await client.sendTransacEmail({
+      sender: { email: process.env.EMAIL },
+      to: [{ email: user.email }],
+      subject: "Delivery OTP",
+      htmlContent: `<p>Your OTP for delivery is <b>${otp}</b>. It expires in 5 minutes.</p>`
     });
-    } catch (error) {
-        console.log("MAIL ERROR:", error);
-    }
+
+    console.log("MAIL SENT ✅");
+
+  } catch (error) {
+    console.log("BREVO ERROR:", error);
+    throw error;
+  }
 };
 
